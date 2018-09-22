5c44a1511fa92a3d5ea8a714f31e1d549f8c6873
Thiago Dantas <thiagodma52@gmail.com>
2018-09-22 16:12:29 -0300

    adicionado script para separar os dados entre folhas complexas e simples

---
+++ getSeparateData.m
@@ -0,0 +1,26 @@
+clear all;
+close all;
+clc
+
+dataset = csvread('leaf.csv');
+% dataset = dataset(randperm(size(dataset,1)),:);
+Labels = dataset(:,1);
+Features = dataset(:,3:end);
+
+%simple leaves: classes 1:15 and 22:36
+
+simpleLabels = [];
+simpleFeatures = [];
+
+for i=1:15
+    idx = Labels == i;
+    simpleLabels = [simpleLabels ; Labels(idx)];
+    simpleFeatures = [simpleFeatures ; Features(idx)];
+    Labels(idx) = [];
+    Features(idx,:) = [];
+end
+
+complexFeatures = Features;
+complexLabels = Labels;
+
+clear i idx;
