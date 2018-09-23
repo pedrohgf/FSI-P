465e99fb3e6cd9cb66fd2f85acb466e29ecea223
pedrohgf <37408930+pedrohgf@users.noreply.github.com>
2018-09-23 12:38:47 -0300

    Adicionadas novas Métricas
    Confira se chegou certo. Abs!

--- RandomForest.m
+++ RandomForest.m
@@ -4,7 +4,12 @@ Labels = dataset(:,1);
 Features = dataset(:,3:end);
 NumberOfPartitions = 10;
 Partitions = cvpartition(1:length(dataset), 'KFold', NumberOfPartitions);
+CorrectPrediction = 0;
 
+TrueLabels = [];
+GuessedLabels = [];
+NumberOfDecisionTrees = 18;
+    
 for i=1:NumberOfPartitions
     TSIndex = training(Partitions, i);
     TrainingSetLabels = Labels(TSIndex);
@@ -12,21 +17,22 @@ for i=1:NumberOfPartitions
     TestIndex = test(Partitions, i);
     TestsetLabels = Labels(TestIndex);
     Testset = Features(TestIndex,:);
-    Model = TreeBagger(50, TrainingSet, TrainingSetLabels);
+    Model = TreeBagger(NumberOfDecisionTrees, TrainingSet, TrainingSetLabels);
     LabelsPredicted = predict(Model, Testset);
-    keyboard;
+    LabelsPredicted = cellfun(@str2num, LabelsPredicted, 'UniformOutput', false);
+    LabelsPredicted = cell2mat(LabelsPredicted);
+    CorrectPrediction = CorrectPrediction + sum(LabelsPredicted == TestsetLabels);
+    TrueLabels = [TrueLabels TestsetLabels'];
+    GuessedLabels = [GuessedLabels LabelsPredicted'];
 end
 
-% TestRatio = 0.1;
-% OffsetRatio = 0;
-% NumberOfSamples = length(dataset);
-% TestsetIndex = OffsetRatio+1:OffsetRatio+1+TestRatio*NumberOfSamples;
-% Testset = Features(TestsetIndex,:);
-% TestsetLabels = Labels(TestsetIndex);
-% TrainingSetIndex = setdiff(1:NumberOfSamples, TestsetIndex);
-% TrainingSet = Features(TrainingSetIndex, :);
-% TrainingSetLabels = Labels(TrainingSetIndex);
+ClassRightPredicted = [];
+ClassWrongPredicted = [];
+NumberOfClasses = 40;
+for i=1:NumberOfClasses
+    IDX = find(TrueLabels == i);
+    ClassRightPredicted(i) = sum(GuessedLabels(IDX) == i);
+    ClassWrongPredicted(i) = length(IDX) - ClassRightPredicted(i);
+end
 
-Model = TreeBagger(50, TrainingSet, TrainingSetLabels);
-LabelsPredicted = predict(Model, Testset); 
-% Model = TreeBagger(50, 
+CorrectPredictionRatio = ClassRightPredicted./(ClassRightPredicted+ClassWrongPredicted);
